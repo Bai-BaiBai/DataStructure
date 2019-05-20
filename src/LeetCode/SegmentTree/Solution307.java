48379bd6174e381424f965bafbfb1c2260700815
Bai-BaiBai <877049473@qq.com>
2019-05-20 16:17:53 +0800

    307号问题线段树解决

--- src/LeetCode/SegmentTree/Solution307.java
+++ src/LeetCode/SegmentTree/Solution307.java
@@ -6,8 +6,8 @@ import SegmentTree.SegmentTree;
  * 题目要求与303号问题大致相同
  * 给定一个数组(可变)，求出数组从索引i到j范围内元素的总和，包含i,j两点
  * 可以使用update操作更改数组中的值
- * 如果使用额外开辟一个数组存储前i个元素的值的方式，时间复杂度为O(n)，会造成超时
- * 可以使用线段树的方式，时间复杂度为O(logn)
+ * 如果使用额外开辟一个数组存储前i个元素的值的方式，每次update时间复杂度为O(n)整体为O(mn)，会造成超时
+ * 可以使用线段树的方式，update时间复杂度为O(logn)整体为O(mlogn)
  */
 public class Solution307 {
 
