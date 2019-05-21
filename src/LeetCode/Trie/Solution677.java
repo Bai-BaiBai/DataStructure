0c7d1fe65ee92db4cdfc24cc16520758650ee26e
Bai-BaiBai <877049473@qq.com>
2019-05-21 21:25:41 +0800

    LeetCode677号问题Trie解决

--- src/LeetCode/Trie/Solution677.java
+++ src/LeetCode/Trie/Solution677.java
@@ -61,9 +61,10 @@ public class Solution677 {
     }
 
     //这种求和方式需要在调用手动判断prefix的结尾字符是不是一个单词的结尾
+    //这个的策略是node节点的value在上一步就已经添加了，然后取递归它的后面节点
     //不知道为什么，提交答案时，这种方案比下面那种方案时间和内存消耗都小一点点
     private int __sum(Node node){
-        int res = 0;
+        int res = 0;//该节点的值在上一步已经添加完成了
         for (Node cur : node.next.values()){
             if (cur.isWord){
                 res += cur.value;
@@ -74,6 +75,7 @@ public class Solution677 {
     }
 
     /*
+    //这种策略是node节点的值在当前过程添加，然后去递归后面的节点
     //这种求和方式不需要使用node中的isWord标志位，因为value=0就能表明该节点不是字符串结尾
     private int __sum(Node node){
         int res = node.value;
