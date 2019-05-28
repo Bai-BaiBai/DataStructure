8aeac8c4ccf15532b6f3e13cd91a53ddc9e19d19
Bai-BaiBai <877049473@qq.com>
2019-05-28 19:08:03 +0800

    哈希表的实现

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -9,24 +9,26 @@ import java.util.TreeMap;
  */
 public class HashTable<K, V> {
 
+    private final int[] capacity
+            = {53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593,
+            49157, 98317, 196613, 393241, 786433, 1572869, 3145739, 6291469,
+            12582917, 25165843, 50331653, 100663319, 201326611, 402653189, 805306457, 1610612741};
+
     private static final int upperTol = 10;
     private static final int lowerTol = 2;
-    private static final int initCapacity = 7;
+    private static int capacityIndex = 0;
 
     private TreeMap<K, V>[] hashtable;
     private int M;//哈希表的长度
     private int size;
 
-    public HashTable(int M){
-        this.M = M;
+    public HashTable(){
+        this.M = capacity[capacityIndex];
         this.size = 0;
         this.hashtable = new TreeMap[M];
         for (int i = 0; i < M; i++) {
             hashtable[i] = new TreeMap<>();
         }
-    }
-    public HashTable(){
-        this(initCapacity);
     }
 
     //根据hash值定位
@@ -46,8 +48,8 @@ public class HashTable<K, V> {
             map.put(key, value);
             size++;
 
-            if (size >= upperTol * M){
-                resize(M*2);
+            if (size >= upperTol * M && capacityIndex+1 < capacity.length){
+                resize(capacity[++capacityIndex]);
             }
         }
     }
@@ -59,8 +61,8 @@ public class HashTable<K, V> {
             ret = map.remove(key);
             size --;
 
-            if (size < lowerTol * M && M >= 14){
-                resize(M/2);
+            if (size < lowerTol * M && M >= 14 && capacityIndex-1 > 0){
+                resize(capacity[--capacityIndex]);
             }
         }
         return ret;
